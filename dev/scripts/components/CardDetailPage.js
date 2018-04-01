ba2e73d85465d13b26492ee0f6ecf22850973b8f
Ferdinand Ismael <ferdinandismael@Ferdinands-MacBook-Pro.local>
2018-04-01 08:57:33 -0400

    added log in, formatting cardDisplay, added some styling

--- dev/scripts/components/CardDetailPage.js
+++ dev/scripts/components/CardDetailPage.js
@@ -13,6 +13,7 @@ class CardDetailPage extends React.Component {
         }
 
         this.getCardInfo = this.getCardInfo.bind(this);
+        this.addToDeck = this.addToDeck.bind(this);
     }
 
     componentDidMount() {
@@ -20,7 +21,7 @@ class CardDetailPage extends React.Component {
         this.setState({ cardId : this.props.match.params.cardId },
             () => {
                 // wait until state is set before making axios call
-                this.getCardInfo();
+                this.getCardInfo()
             }
         )
     }
@@ -28,20 +29,99 @@ class CardDetailPage extends React.Component {
     getCardInfo() {
         axios.get(`${credentials.pokemonApiUrl}/cards/${this.state.cardId}`)
             .then((res) => {
-                this.setState({
-                    cardInfo : res.data.card,
-                })
+                this.setState({ cardInfo: res.data.card },() => this.render());
             });
     }
 
+    addToDeck() {
+      console.log("clicked")
+      console.log(this.state.cardInfo)
+      const dbRefUser = firebase.database().ref(`users/${firebase.auth().currentUser.uid}`);
+      console.log(dbRefUser);
+      console.log(this.state.cardInfo)
+      // dbRefUser
+      // .push({
+      //    start: this.props.location.state.firstChoice,
+      //    end: this.props.location.state.endChoice,
+      //    startTime: this.state.startTime
+      // })
+      // .then((data) => {
+      //    const dbRefPublic = firebase.database().ref(`/public/${data.ref.key}`);
+      //    console.log(dbRefPublic);
+      //    dbRefPublic.update({
+      //       start: this.props.location.state.firstChoice,
+      //       end: this.props.location.state.endChoice,
+      //       startTime: this.state.startTime
+      //    });
+      // });
+    }
+
     render() {     
-        console.log(this)   
+        const { ability, attacks, hp, name, types, weaknesses, imageUrl, rarity, supertype, text } = this.state.cardInfo;
+        console.log(attacks)
         return (
-            <main>
-                <p>Card detail page</p>
-                {/* <p>{this.state.cardId}</p> */}
-                <a href="" onClick={this.props.history.goBack}>ihihih</a>
-            </main>
+            <React.Fragment>
+               <p>Card detail page</p>
+               {/* <a href="" onClick={this.props.history.goBack}>ihihih</a> */}
+
+               <main className="cardDetails">
+
+                  <aside className="detailsImg">
+                     <img src={imageUrl} alt={`a picture of ${name}`}/>
+                  </aside>
+
+                  <section className="detailsContent">
+                  
+                     <div className="detailsContainer">
+                        <h2>
+                           {name}
+                           <span>
+                              {supertype}
+                           </span>
+                        </h2>
+                        {
+                           supertype === "Pokémon" 
+                           ?  <h2>
+                                 HP{hp}
+                                 <img src={`../../../images/${types}.png`} alt={`an emblem of the type ${types}`} />
+                              </h2>
+                           : null
+                        }  
+                     </div>
+
+                     <p>{text}</p>
+
+                     <h3>
+                        {`rarity: "${rarity}"`}
+                     </h3>
+
+                     {
+                        attacks
+                        ? attacks.map((attack, i) => {
+
+                           return   <React.Fragment key={i}>
+                                       <h2>
+                                          {attack.name}
+                                          {
+                                             attack.cost 
+                                             ? attack.cost.map((cost,i) => {
+                                                return <img key={i} src={`../../../images/${cost}.png`} alt={`an emblem of the type ${types}`}/>
+                                             })
+                                             :null
+                                          }
+                                       </h2>
+                                       <p>{attack.text}</p>
+                                    </React.Fragment>;
+                        })
+                        : null
+                     }
+
+                     <button onClick={this.addToDeck}>Add to Deck</button>
+
+
+                  </section>
+               </main>
+            </React.Fragment>
         )
     }
 };
